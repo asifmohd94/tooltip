cbd70f97c9a4a06d091d3887a4f6719e35c9b8c1
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-10-30 12:19:33 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,41 @@
+import React, { Component } from 'react'
+import './App.css';
+import Tooltip from './components/Tooltip';
 
+
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      input: ""
+    }
+  }
+
+
+  handleInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Input position of tooltip</h1><br></br>
+        <label>Input here: </label>
+        <input
+          type='text'
+          value={this.state.input}
+          onChange={this.handleInput}
+        /><br></br>
+        <span>Default tooltip position is top</span>
+        <Tooltip pos={this.state.input} />
+      </div>
+    )
+  }
+}
+
+export default App
